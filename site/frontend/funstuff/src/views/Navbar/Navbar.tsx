ad821f1f43e794a2437642ffb9ed11c309a0b51c
aaron <aaron777collins@gmail.com>
2022-07-24 19:35:58 -0400

    Fixed navbar account colours

--- site/frontend/funstuff/src/views/Navbar/Navbar.tsx
+++ site/frontend/funstuff/src/views/Navbar/Navbar.tsx
@@ -35,6 +35,35 @@ const Navbar = () => {
     setAnchorElUser(null);
   };
 
+  function stringAvatar(name: string) {
+    return {
+      sx: {
+        bgcolor: stringToColor(name),
+      },
+      children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+    };
+  }
+
+  function stringToColor(string: string) {
+    let hash = 0;
+    let i;
+  
+    /* eslint-disable no-bitwise */
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+  
+    let color = '#';
+  
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
+    }
+    /* eslint-enable no-bitwise */
+  
+    return color;
+  }
+
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
@@ -128,8 +157,8 @@ const Navbar = () => {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                {/* <Avatar alt="Aaron Collins" src=""/> */}
+                {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" /> */}
+                <Avatar alt="Aaron Collins" {...stringAvatar('Aaron Collins')}/>
               </IconButton>
             </Tooltip>
             <Menu
