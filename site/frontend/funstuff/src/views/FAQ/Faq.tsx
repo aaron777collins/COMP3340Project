20e60de5152225f1fc5691a911fff1911fe8c143
kevinliu8896 <kevinliu8896@gmail.com>
2022-07-25 21:16:35 -0400

    Added FAQ page

--- site/frontend/funstuff/src/views/FAQ/Faq.tsx
+++ site/frontend/funstuff/src/views/FAQ/Faq.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { getLogger } from "../../LogConfig";
 import logo from "../../logo.svg";
+import "./Faq.css";
 
 // Used for adding parameters to the Faq tag
 export interface IFaqProps {}
@@ -33,26 +34,46 @@ export default function Faq(props: IFaqProps) {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header"></header>
+      <body className="App-body">
+        <h1>FAQ Page</h1>
+        <div className="App-questions">
+          <p>How can I contact Fun Stuff?</p>
+          <div>
+            <li>You can email us at a info@funstuff.com where our customer service team will be happy to help you with whatever you may need.</li>
+            <p>Do you ship worldwide?</p>
+            <li>Yes.</li>
+            <p>Where do you ship from?</p>
+            <li>We ship from Canada.</li>
+            <p>Can I change or cancel my order?</p>
+            <li>As we aim to process as fast as possible, you must request any changes/cancellations within 12 hours of ordering. 
+              All requests after this time will be denied. Your order can be returned for a full refund after it is received.</li>
+            <p>What payment methods do you accept?</p>
+            <li>We accept all major credit cards (VISA, Mastercard, AMEX) and PayPal payments.</li>
+            <p>When will my order be processed?</p>
+            <li>All orders are handled and shipped out from our warehouse. 
+              Please allow extra time for your order to be processed during holidays and sale seasons. 
+              We process orders between Monday and Friday. 
+              Orders will be processed within 3-5 business days from the order date and shipped the next day after the processing day. 
+              Please note that we don't ship on weekends.</li>
+            <p>How long will it take to receive my order?</p>
+            <li>Once you place your order, please allow 3-5 business days to process your orders. After that, it will take 7-14 business days for delivery.</li>
+            <p>How do I return a product?</p>
+            <li>Please contact us at info@funstuff.com</li>
+            <p>What if the item(s) I received are defective/incorrect/damaged?</p>
+            <li>Please contact us if you have received merchandise that is incorrect, missing, and/or defective. 
+              Please include your order number, photographs of the item(s) and all related references upon receiving your package. 
+              We will do our very best to resolve your case as soon as possible.</li>
+            <p>When will I receive my refund?</p>
+            <li>All refunds will be credited to your original form of payment.
+              If you paid by credit or debit card, refunds will be sent to the card-issuing bank within 7-10 business days of receipt of the returned item or cancellation request. 
+              Please contact the card-issuing bank with questions about when the credit will be posted to your account. 
+              If you haven't received a credit for your return yet, here's what to do: Contact the bank/credit card company. 
+              It may take some time before the refund is posted to your account.</li><br></br>
+          </div>
 
-        <p>The FAQ</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <form action="../../post" method="post" className="form">
-          <button type="submit">Connected?</button>
-        </form>
-        <button type="submit" onClick={getData}>
-          Test Api
-        </button>
-        <div>{response.resp}</div>
-      </header>
+        </div>
+      </body>
     </div>
   );
 }
--- site/frontend/funstuff/src/views/Products/Products.tsx
+++ site/frontend/funstuff/src/views/Products/Products.tsx
@@ -34,25 +34,17 @@ export default function Products(props: IProductsProps) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <body className="poroductsClass">
+        <h1>Featured Products</h1>
+          <div className="product-details">
+          <p>Gallery</p>
+            <p>product info</p>
 
-        <p>The Products</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <form action="../../post" method="post" className="form">
-          <button type="submit">Connected?</button>
-        </form>
-        <button type="submit" onClick={getData}>
-          Test Api
-        </button>
-        <div>{response.resp}</div>
+          </div>
+        
+      </body>
       </header>
+
     </div>
   );
 }
