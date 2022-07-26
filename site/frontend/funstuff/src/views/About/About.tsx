e7adf293c05473858cbc57169c564a34069be744
brugbug <brugbug001@gmail.com>
2022-07-25 20:37:34 -0400

    Made about page

--- site/frontend/funstuff/src/views/About/About.tsx
+++ site/frontend/funstuff/src/views/About/About.tsx
@@ -2,8 +2,14 @@ import { getByDisplayValue } from "@testing-library/react";
 import React, { useEffect, useState } from "react";
 import { getConfigFileParsingDiagnostics } from "typescript";
 import { getLogger } from "../../LogConfig";
-import logo from "../../logo.svg";
 import "./About.css";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const log = getLogger("view.about");
 
@@ -34,37 +40,70 @@ function About() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <p>The about page!</p>
-        {getCards()}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <form action="../../post" method="post" className="form">
-          <button type="submit">Connected?</button>
-        </form>
-        <button type="submit" onClick={getData}>
-          Test Api
-        </button>
-        <div>{response.resp}</div>
+        <h1>About Us</h1>
       </header>
+      <body className="App-body">
+        <h2>What We Do</h2>
+        <p>Fun Stuff is an e-commerce website specializing in the funky stuff you <i>wish</i> you could find at a gift shop! Whether 
+          it's a Foot Stroker or a Bosnian Booze Bunker, we have it all!</p>
+        <h2>The Fun Stuff Crew</h2>
+        {BasicTable()}
+        <h2>Location</h2>
+        <p>1234 Chungarius Ave. South</p>
+        <p>Windsor, ON N1A 2B3</p>
+        <h2>Contact Us</h2>
+        <p>	&#40;519&#41; 123-4567</p>
+
+      </body>
     </div>
   );
 }
 
 export default About;
-function getCards(): React.ReactNode {
-  const getDivs = () => {
-    let arr = ["about", "me"];
-    return arr.map((elem, ind) => {
-      return <p key={ind}>{elem}</p>;
-    });
-  };
-  return <>{getDivs()}</>;
+
+function createData(
+  name: string,
+  description: string,
+  email: string,
+) {
+  return { name, description, email };
+}
+
+const rows = [
+  createData('Aaron Collins', 'temp@peameal.bacon', 'Loves working on our backend ;)'),
+  createData('Alec Mladenovic', 's', 's'),
+  createData('Aleksa Vinjevic', 's', 's'),
+  createData('Kevin Liu', 's', 's'),
+  createData('Riley OKeefe', 's', 's'),
+];
+
+export function BasicTable() {
+  return (
+    <TableContainer component={Paper} className="GroupMemberTable">
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Group Member</TableCell>
+            <TableCell align="right">E-Mail</TableCell>
+            <TableCell align="right">Description</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.description}</TableCell>
+              <TableCell align="right">{row.email}</TableCell>
+
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
