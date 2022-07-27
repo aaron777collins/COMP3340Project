73915f8cfaefadf738a62d783cfa2376c48374d2
kevinliu8896 <kevinliu8896@gmail.com>
2022-07-26 23:09:24 -0400

    Added checkout page currently working on

--- site/frontend/funstuff/src/views/App.tsx
+++ site/frontend/funstuff/src/views/App.tsx
@@ -5,6 +5,7 @@ import About from "./About/About";
 import Navbar from "./Navbar/Navbar";
 import Products from "./Products/Products";
 import Faq from "./FAQ/Faq";
+import Checkout from "./Checkout/Checkout";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/products" element={<Products />} />
           <Route path="/faq" element={<Faq />} />
+          <Route path="/checkout" element={<Checkout />} />
         </Routes>
       </BrowserRouter>
     </>
