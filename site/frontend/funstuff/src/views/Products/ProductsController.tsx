88fb544bb4e79062e6c25527b99a2a2119fa6590
aaron <aaron777collins@gmail.com>
2022-07-28 22:33:59 -0400

    Added loading backdrop

--- site/frontend/funstuff/src/views/Products/ProductsController.tsx
+++ site/frontend/funstuff/src/views/Products/ProductsController.tsx
@@ -1,24 +1,30 @@
-import axios from 'axios';
-import * as React from 'react';
-import { useEffect, useState } from 'react';
-import { CartItemModel, ItemModel } from '../../Models/Item';
-import { ProductObj } from '../Product/Product';
-import Products from './Products';
+import { Backdrop, CircularProgress } from "@mui/material";
+import axios from "axios";
+import * as React from "react";
+import { useEffect, useState } from "react";
+import { CartItemModel, ItemModel } from "../../Models/Item";
+import { ProductObj } from "../Product/Product";
+import Products from "./Products";
 
 interface ProductsDataObj {
-    productsArray: ProductObj[],
+  productsArray: ProductObj[];
 }
 
 interface IProductsController {
-  items: CartItemModel[],
-  setItems: Function
+  items: CartItemModel[];
+  setItems: Function;
 }
 
 export default function ProductsController(props: IProductsController) {
 
-    function getItems() {
-      axios.get(process.env.REACT_APP_DBAPI_ADDRESS_BEGINNING + 'getAllItems').then((resp) => {
-        const {data} = resp;
+  const [loading, setLoading] = useState(false);
+
+  function getItems() {
+    setLoading(true);
+    axios
+      .get(process.env.REACT_APP_DBAPI_ADDRESS_BEGINNING + "getAllItems")
+      .then((resp) => {
+        const { data } = resp;
         let items = data.resp as ItemModel[];
         let products = items.map((item) => {
           return {
@@ -27,22 +33,33 @@ export default function ProductsController(props: IProductsController) {
             Description: item.description,
             Price: item.price,
           } as ProductObj;
-        })
-        setProductData({productsArray: products} as ProductsDataObj);
+        });
+        setProductData({ productsArray: products } as ProductsDataObj);
+        setLoading(false);
       });
-    }
+  }
 
-    useEffect(getItems, []);
+  useEffect(getItems, []);
 
-    const initialProductsObject: ProductsDataObj = {
-        productsArray: []
-    }
-    
-    const [productData, setProductData]=useState(initialProductsObject);
+  const initialProductsObject: ProductsDataObj = {
+    productsArray: [],
+  };
+
+  const [productData, setProductData] = useState(initialProductsObject);
 
   return (
     <div>
-      <Products productsData={productData.productsArray} items={props.items} setItems={props.setItems}></Products>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={loading}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <Products
+        productsData={productData.productsArray}
+        items={props.items}
+        setItems={props.setItems}
+      ></Products>
     </div>
   );
 }
