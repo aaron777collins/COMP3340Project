7e51bfced0435e05201911a333194f255238983b
brugbug <brugbug001@gmail.com>
2022-08-08 16:31:53 -0400

    Changed name of Gatorade product
    Added product images 800 x 800
    
    Changed the redirects on the legal page

--- site/frontend/funstuff/src/views/Legal/Legal.tsx
+++ site/frontend/funstuff/src/views/Legal/Legal.tsx
@@ -24,7 +24,12 @@ export default function Legal(props: ILegalComponentProps) {
         <h1>Legal Information</h1>
         <div className="Legal-questions">
           <div>
-            You can find all of our legal information at the following pages:
+            You can find all of our legal information at the following pages:<br></br><br></br>
+            <ul>
+              <li><a href="./terms" target="_blank">Terms and Conditions<br></br><br></br></a></li>
+              <li><a href="./privacy" target="_blank">Privacy Policy<br></br><br></br></a></li>
+              <li><a href="./refund" target="_blank">Refund Policy</a></li>
+            </ul>
           </div>
         </div>
       </body>
