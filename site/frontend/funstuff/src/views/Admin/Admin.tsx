ba64a50625b3fdab349c871ee33d944a07c13223
brugbug <brugbug001@gmail.com>
2022-08-09 21:50:47 -0400

    Changed margins between the cards

--- site/frontend/funstuff/src/views/Admin/Admin.tsx
+++ site/frontend/funstuff/src/views/Admin/Admin.tsx
@@ -91,7 +91,7 @@ export default function Admin() {
   function getGridCards() {
     return functionObjArr.map((funcObj) => (
       <Grid item key={funcObj.name}>
-        <Card sx={{ minWidth: 200, mt: 5, ml: 5 }}>
+        <Card sx={{ minWidth: 200 }}>
           <CardContent sx={{ "&:last-child": { mb: 0, pb: 0} }}>
             <Typography sx={{ fontSize: 24, fontWeight: "bold" }}>
               {funcObj.name}
@@ -115,11 +115,11 @@ export default function Admin() {
 
   return (
     <>
-      <Grid container spacing={3}>
+      <Grid container spacing={3} sx={{ ml: 2, mt: 5 }}>
         {getGridCards()}
       </Grid>
       
-      <Card sx={{ minWidth: 200, width: '90%', mt: 5, ml: 5 }}>
+      <Card sx={{ minWidth: 200, width: '95%', mt: 3, ml: 5 }}>
         <CardContent>
           <Typography sx={{ fontSize: 24, fontWeight: "bold" }}>
             Output
@@ -129,11 +129,12 @@ export default function Admin() {
           </Typography>
         </CardContent>
       </Card>
-      <Card sx={{ minWidth: 200, width: '30%', mt: 5, ml: 5, mb: 5 }}>
+      <Card sx={{ minWidth: 100, width: '25%', mt: 3, ml: 5, mb: 5 }}>
         <CardContent>
           <Typography sx={{ fontSize: 24, fontWeight: "bold" }}>
             Admin Documentation
           </Typography>
+          <br></br>
           <Button href="./adminDocumentation" variant="contained">Click here to visit the Admin Documentation page</Button>
         </CardContent>
       </Card>
