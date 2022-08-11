2bf3fb9ff700f474c175315efeb6187bbdf61f0c
aaron <aaron777collins@gmail.com>
2022-08-10 21:23:44 -0400

    Moved to selenium testing for frontend

--- site/frontend/funstuff/src/views/Home/Home.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Home from './Home';
-
-// test('renders learn react link', () => {
-//   render(<Home loading={}/>);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
